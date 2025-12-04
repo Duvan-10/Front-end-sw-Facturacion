3e8c6fe27f3019086f9c50c2f327778e63520d93
Duvan-Melo <duvan.10dma@gmail.com>
2025-12-03 19:30:44 -0500

    Login

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="app-container">
+      {/* Aquí irán tus rutas o tu componente de Login/Dashboard */}
+      <h1></h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css' // Mantiene la importación de tu hoja de estilos principal
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
