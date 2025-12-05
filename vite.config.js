1cbe19dd15fad6fe816a28ada2ece305d38b4c50
Duvan-Melo <duvan.10dma@gmail.com>
2025-12-05 00:29:23 -0500

    Modulo Login

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
+// Front-end-sw-Facturacion/vite.config.js
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 👈 CRÍTICO: Indica a Vite que la raíz del proyecto web está aquí
+  root: 'Front-end', 
 })
