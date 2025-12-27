13d02518d465bc481c26e0bc6256f2601a0a09d3
Duvan-10 <duvan.10dma@gmail.com>
2025-12-27 10:46:03 -0500

    Scritp Reportes bot

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,34 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+ // Todo se guarda fuera del proyecto (en el disco D:)
+  // Creamos una subcarpeta 'temp' para que el plugin tenga de donde copiar
+  videosFolder: 'D:/Videos/Pruebas Calidad/videos',
+  screenshotsFolder: 'D:/Videos/Pruebas Calidad/screenshots',
+  downloadsFolder: 'D:/Videos/Pruebas Calidad/downloads',
+
   reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    // El reporte final (HTML) quedará en la raíz de tu carpeta de Calidad
+    reportDir: 'D:/Videos/Pruebas Calidad',
+    autoOpen: true,
+    charts: true,
+    embeddedScreenshots: true,
+    inlineAssets: false, 
+    saveAllAttempts: false,
+
+    // 4. Forzamos que se mantenga simple
+    overwrite: true,
+    html: true,
+    json: false // No necesitamos los JSON una vez hecho el reporte
+  },
+  
   e2e: {
-baseUrl: 'http://localhost:5173',
+    baseUrl: 'http://localhost:5173',
+    video: true, // Habilita la grabación de videos
+    screenshotOnRunFailure: true, // También toma fotos si algo falla
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
+    require('cypress-mochawesome-reporter/plugin')(on);
     },
     browsers: [
       {
